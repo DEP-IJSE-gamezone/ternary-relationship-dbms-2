b7b68ac7997210c2ac77ebe302694f7ce9eb69f2
diliniapsara <deegaladil@gmail.com>
2024-07-29 10:43:22 +0530

    modify mudule-2

--- src/main/java/lk/ijse/dep12/relationship/jpa/entity/Module.java
+++ src/main/java/lk/ijse/dep12/relationship/jpa/entity/Module.java
@@ -27,32 +27,5 @@ public class Module implements Serializable {
         Optional, Mandatory
     }
 
-    @ManyToMany(cascade = CascadeType.PERSIST)
-    @JoinTable(name = "course_module",
-    joinColumns = @JoinColumn(name = "module_code",referencedColumnName = "code"),
-    inverseJoinColumns = @JoinColumn(name = "course_code",referencedColumnName = "code"))
-    private List<Course> courses=new ArrayList<>();
-
-
-    @ManyToMany(cascade = CascadeType.PERSIST)
-    @JoinTable(name = "take",
-    joinColumns = @JoinColumn(name = "module_code",referencedColumnName = "code"),
-    inverseJoinColumns = @JoinColumn(name = "student_nic",referencedColumnName = "nic"))
-    private List<Student> students=new ArrayList<>();
-
-    @ManyToMany
-    @JoinTable(name = "take",
-            joinColumns =  @JoinColumn(name = "module_code",referencedColumnName = "code"),
-            inverseJoinColumns =@JoinColumn(name = "batch_id",referencedColumnName = "batchId"))
-    private List<Module> module=new ArrayList<Module>();
-
-
-
-    public Module(String code, String name, BigDecimal credit, Type type) {
-        this.code = code;
-        this.name = name;
-        this.credit = credit;
-        this.type = type;
-    }
 
 }
